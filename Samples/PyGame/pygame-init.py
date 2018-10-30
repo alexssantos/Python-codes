68053098126d2c6da63d8ae064ebfd4ec22f4865
Alex Santos <arkanjo.alex@gmail.com>
2018-10-30 08:22:43 -0300

    pygame - minimo para iniciar janela

--- Samples/PyGame/pygame-init.py
+++ Samples/PyGame/pygame-init.py
@@ -6,4 +6,16 @@ largura_tela = 1280
 altura_tela = 720
 tela = pygame.display.set_mode((largura_tela, altura_tela))
 
+finish = False
+while not finish:
+
+    #atualiza
+    pygame.display.update()
+
+    #checar os 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
+
+pygame.display.quit()
 pygame.quit()
