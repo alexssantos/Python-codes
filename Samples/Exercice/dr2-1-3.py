a7f44cc0d94893f6eeedae684f2e8bd9b915b86d
Alex Santos <arkanjo.alex@gmail.com>
2018-11-06 00:50:48 -0200

    fixed exe 3

--- Samples/Exercice/dr2-1-3.py
+++ Samples/Exercice/dr2-1-3.py
@@ -1,6 +1,8 @@
 n = int(input("Calcular o Fatorial de :"))
 
-n = abs(n)  # somente positivo
+if n < 0:
+    print(n, "é menor que 0. Namos calcular: ", -n, "!")
+    n = abs(n)  # somente positivo
 
 result = 1
 for numb in range(1, n+1):
