4e59e666d0c9ff4a9e7f393e186858764950a4eb
Alex Santos <arkanjo.alex@gmail.com>
2018-11-06 22:48:45 -0200

    exercice 12

--- Samples/Exercice/dr2-1-11.py
+++ Samples/Exercice/dr2-1-11.py
@@ -8,3 +8,4 @@ for x in range(1, 4):
     turtle.forward(lado)
     turtle.setheading(120*x)
 
+turtle.Screen().mainloop()
---
+++ Samples/Exercice/dr2-1-12.py
@@ -0,0 +1,6 @@
+import turtle
+
+raio = int(input("Digite o valor [20 ~ 50]  do Raio:  "))
+turtle.circle(raio*5)
+
+turtle.Screen().mainloop()
