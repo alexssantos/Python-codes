65b9e29b74352cb40c4dc2f66e1b4091aa9adf99
Alex Santos <arkanjo.alex@gmail.com>
2018-11-16 10:22:31 -0200

    aula 16/11/18 - projeto - exercicios etapa4

---
+++ Projects/etapa4-exercises.py
@@ -0,0 +1,21 @@
+import os 
+
+#lsitar diretorios
+os.listdir()
+
+#validação Se É arquivo 
+os.isfile()
+
+
+
+
+# Exercício 1 
+#Tente criar um dicionário onde a chave é o nome do arquivo e o valor é uma lista contendo os 
+#   dados: tamanho do arquivo, data de criação e data de modificação.
+
+
+
+# Exercício 2
+# Tente realizar a impressão dos dados dos arquivos presente no dicionário criado no exercício anterior de forma formatada e alinhada.
+# obs:O Código 2, resposta do exercício 2, representa a continuação do Código 1 para mostrar a tabela formatada com alinhamento:
+
