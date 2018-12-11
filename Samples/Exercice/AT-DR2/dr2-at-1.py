ac80febc8dd055cc770df73fadea47285f4ed1a1
Alex Santos <arkanjo.alex@gmail.com>
2018-12-11 00:01:49 -0200

    updates

--- Samples/Exercice/AT-DR2/dr2-at-1.py
+++ Samples/Exercice/AT-DR2/dr2-at-1.py
@@ -1,12 +1,28 @@
-t = tuple(str.split(input(print('Digite 3 numeros: '))))
-t = [int(x) for x in t]
+# t = tuple(str.split(input(print('Digite 3 numeros: '))))
+# t = [int(x) for x in t]
 
-aux = []
-for i in t:
-    for x in t:
-        if i > x:
-            aux.insert(0, i)
+# myTuple = (n1, n2, n3)
+# aux = sorted(myTuple)
+
+# myTuple = tuple(n1, n2, n3)
+# aux = []
+# for i in t:
+#     for x in t:
+#         if i > x:
+#             aux.insert(0, i)
+
+n1 = 45
+n2 = 23
+n3 = 36
+
+myTuple = (n1, n2, n3)
+
+# aux = []
+# for i in myTuple:
+#     for x in myTuple:
+#         if i > x:
+#             aux.insert(x, i)
     
 
-print(t)
+print(myTuple)
 print(aux)
