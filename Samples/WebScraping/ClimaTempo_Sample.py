5abf3c56125499207e6a5b1054fd8d1bfb67cb67
Alex Santos <arkanjo.alex@gmail.com>
2018-12-13 20:23:23 -0200

    Modulo PrettyTable add

--- Samples/WebScraping/ClimaTempo_Sample.py
+++ Samples/WebScraping/ClimaTempo_Sample.py
@@ -7,7 +7,7 @@ url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/mavg/tas/1
 csv = requests.get(url).text
 
 linhas = csv.splitlines()   # list<string> - Cada string com info separadas por ','
-header = linhas[0].split()
+header = linhas[0].split(',')
 dataLists = [x for x in linhas[1:]]
 
 
@@ -18,24 +18,17 @@ def LinesAndColuns(linhas):
         '\n-------------------------\n')
 
     for lin in linhas:      # lin = string - info separadas por ','
-        index = linhas.index(lin) + 1
-
-        print(  # cada linha
-            '-------------LINHA %d ------------\n\n' % index,
-            lin, '\n')
-
         colunas = lin.split(',')  # extrai valores separados por virgula
-        print(  # cada coluna
-            '-------------COLUNA %d ------------\n\n' % index,
-            colunas, '\n')
+        print(colunas)
 
 
+# CUIDADO COM A QUANTIDADE DE COLUNAS - PODE FICAR AGLOMERADO
 def PrintTable(_headerList, _dataLists):
-    # _dataLists inclui varias linhas
+    # _dataLists list<string>
 
     _table = PrettyTable(_headerList)
     for line in _dataLists:
-        dataLineList = line.split()
+        dataLineList = line.split(',')
         _table.add_row(dataLineList)
 
     print(_table)
