9472980ca7204e019b72a4a7eb3fc652f1af39b8
aarkanjo <arkanjo.alex@gmail.com>
2019-02-14 00:20:57 -0200

    TP1-DR4 - Qts 15

--- diretorio_exemplo2/testes.py
+++ diretorio_exemplo2/testes.py
@@ -1,11 +1,30 @@
-import os
-import os.path
+import os, os.path, subprocess, psutil, time
 from datetime import datetime
-import subprocess
-import psutil
 
-pid = input('Digite o n° PID:')
-try:
-    p = psutil.Process(pid)
-except Exception:
-    print('PID não existe!')
+while True:        
+    pid = int(input('Digite o n° PID:'))
+    try:
+        p = psutil.Process(pid)
+        print(p)
+        break
+    except Exception:
+        print('PID não existe!')
+
+print(p.name())
+print(p.exe())
+print(p.cwd())
+print(p.status())
+print(p.username())
+print(p.create_time())
+print(time.ctime(p.create_time()))
+print(p.cpu_times())
+print(p.cpu_percent(interval=1.0))
+print(p.cpu_affinity())
+print(p.memory_percent())
+print(p.memory_info())
+print(p.num_threads())
+print(p.threads())
+p.suspend() # Suspender processo
+p.resume() # Voltar de uma suspensão
+p.terminate() # terminar processo
+p.wait(timeout=3) # esperar 3s
