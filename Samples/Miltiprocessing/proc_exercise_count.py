b8ae29168485f3c876eb7636d50408c770f98e8a
Alex Santos <arkanjo.alex@gmail.com>
2019-03-14 09:18:51 -0300

    PROGRESS - erro com multiprocessing

--- Samples/Miltiprocessing/proc_exercise_count.py
+++ Samples/Miltiprocessing/proc_exercise_count.py
@@ -1,7 +1,17 @@
-import multiprocessing
 import time
 import random
+import multiprocessing
 import threading
+
+
+def print_final_result(t_inicio, soma):
+    # Captura tempo final
+    t_fim = float(time.time())
+    t_total = round(t_fim - t_inicio, 2)
+    # Imprime o resultado e o tempo de execução
+    print(f"Soma: {soma}")
+    print(f"Tempo total: {t_total}")
+    return (soma, t_total)
 
 
 # Sequencial
@@ -22,11 +32,8 @@ def count_sequential():
     soma = 0
     for i in lista:
         soma = soma + i
-    
-    t_fim = float(time.time())  # Captura tempo final
-    # Imprime o resultado e o tempo de execução
-    print("Soma:", soma)
-    print("Tempo total:", t_fim - t_inicio)
+
+    print_final_result(t_inicio, soma)
 
 
 def somaThread(lista, soma_parcial, id):
@@ -36,7 +43,7 @@ def somaThread(lista, soma_parcial, id):
     soma_parcial[id] = soma
 
 
-def count_by_threadings():
+def count_by_threadings():    
     N = int(input("Count By Threads - \nEntre com o tamanho do vetor: "))
 
     # Captura tempo inicial
@@ -48,33 +55,78 @@ def count_by_threadings():
     for i in range(N):
         lista.append(random.randint(-50, 51))
 
-    Nthreads = 4 # Número de threads a ser criado
+    Nthreads = 4  # Número de threads a ser criado
 
     # Vetor para salvar a soma parcial de cada thread
     soma_parcial = Nthreads * [0]
     lista_threads = []
 
     for i in range(Nthreads):
-        ini = i * int(N/Nthreads) # início do intervalo da lista
-        fim = (i + 1) * int(N/Nthreads) # fim do intervalo da lista
-        
-        t = threading.Thread(target=somaThread, args=(lista[ini:fim],soma_parcial, i))
-        t.start() # inicia thread
-        
-        lista_threads.append(t) # guarda a thread
+        ini = i * int(N/Nthreads)  # início do intervalo da lista
+        fim = (i + 1) * int(N/Nthreads)  # fim do intervalo da lista
+
+        t = threading.Thread(
+            target=somaThread, 
+            args=(lista[ini:fim], soma_parcial, i))
+        t.start()  # inicia thread
+
+        lista_threads.append(t)  # guarda a thread
 
     for t in lista_threads:
-        t.join() # Espera as threads terminarem
+        t.join()  # Espera as threads terminarem
         soma = 0
         for i in soma_parcial:
             soma = soma + i
 
-    # Captura tempo final
-    t_fim = float(time.time())
-
-    # Imprime o resultado e o tempo de execução
-    print("Soma:", soma)
-    print(f"Tempo total: {round(t_fim - t_inicio, 2)}s")
+    print_final_result(t_inicio, soma)
 
 
+def somaProc(q1, q2):
+    lista = q1.get()
+    soma = 0
+    for i in lista:
+        soma = soma + i
+    q2.put(soma)
+
+
+def count_by_processing():
+    N = int(input("Entre com o tamanho do vetor: "))
+
+    # Captura tempo inicial
+    t_inicio = float(time.time())
+
+    # Gera lista com valores aleatórios
+    lista = []
+    for i in range(N):
+        lista.append(random.randint(-50, 51))
+    NProc = 4  # Número de processos a ser criado
+    # Fila de entrada dos processos
+    q_entrada = multiprocessing.Queue()
+    # Fila de saída dos processos
+    q_saida = multiprocessing.Queue()
+    lista_proc = []
+    for i in range(NProc):
+        ini = i * int(N/NProc)  # início do intervalo da lista
+        fim = (i + 1) * int(N/NProc)  # fim do intervalo da lista
+        q_entrada.put(lista[ini:fim])
+        p = multiprocessing.Process(
+            target=somaProc, 
+            args=(q_entrada, q_saida))        
+        p.start()  # inicia processo
+        lista_proc.append(p)  # guarda o processo
+
+    for p in lista_proc:
+        p.join()  # Espera os processos terminarem
+
+    soma = 0
+    for i in range(0, NProc):
+        soma = soma + q_saida.get()
+
+    print_final_result(t_inicio, soma)
+
+
+count_sequential()
 count_by_threadings()
+
+if __name__ == "__main__":
+    count_by_processing()
