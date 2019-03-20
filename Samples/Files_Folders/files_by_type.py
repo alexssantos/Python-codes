89fa352662c991441e60d27f66a2bbf7e831cc7f
aarkanjo <arkanjo.alex@gmail.com>
2019-03-19 23:32:53 -0300

    File Folder exercises

--- Samples/Files_Folders/files_by_type.py
+++ Samples/Files_Folders/files_by_type.py
@@ -6,21 +6,25 @@ print(os.path.splitext("exemplo.arq.txt"))
 lista = os.listdir()
 
 dic_arq = {}
+list_dir = []
 for i in lista:
     if os.path.isfile(i):
         # pega a extensao
-        ext = os.path.splitext(i)[1]    # TODO: tratar para arquivos ocultos ( .file )
+        # TODO: tratar para arquivos ocultos ( .file )
+        ext = os.path.splitext(i)[1]
 
         # ja add ?
         if not ext in dic_arq:
             dic_arq[ext] = []
         dic_arq[ext].append(i)
+    else:
+        list_dir.append(i)
 
 for key in dic_arq:
-    print('\
-          ', key)
+    print('arquivos ', key)
     for item in dic_arq[key]:
-        print('\
-              ', item)
+        print('\t' + item)
 
-print(dic_arq)
+print('Pastas:')
+for pasta in list_dir:
+    print('\t' + pasta)
