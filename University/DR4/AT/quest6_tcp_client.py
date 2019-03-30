58c97ab8a3932096f9600f6967bf23412a6c0961
Alex Santos <arkanjo.alex@gmail.com>
2019-03-30 14:14:50 -0300

    AT-DR4 quest6: progress

--- University/DR4/AT/quest6_tcp_client.py
+++ University/DR4/AT/quest6_tcp_client.py
@@ -3,3 +3,38 @@
         a) O cliente envia para o servidor o nome de um diretório e recebe a lista de arquivos (apenas arquivos) existente nele.
         b) O servidor recebe a requisição do cliente, captura o nome dos arquivos no diretório em questão e envia a resposta ao cliente de volta.
 '''
+
+import socket
+import pickle
+
+
+# Cria o socket
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 4200
+host = '127.0.0.1'
+
+
+folder_name = input('Entre com o nome da pasta: \n>>> ')
+try:
+    # Tenta se conectar ao servidor
+    soc.connect((host, port))
+    # Envia o nome do arquivo:
+    soc.send(folder_name.encode('ascii'))
+    # Recebe o tamanho do arquivo:
+    data_recv = soc.recv(2048)
+    files_list = pickle.loads(data_recv)
+
+    '''TRATAMENTOS
+    if len(files_list) > 0:
+        # tem arquivos
+        print('Arquivos: ')
+    else:
+        print('Arquivo não encontrado no servidor!')    '''
+
+except Exception as erro:
+    print(str(erro))
+
+# Fecha o socket
+soc.close()
+print(*files_list, sep='\n')
+input("Pressione qualquer tecla para sair...")
