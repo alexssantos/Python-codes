b050fda69bc45750b50a0103617211df09d54d9c
Alex Santos <arkanjo.alex@gmail.com>
2019-03-31 01:03:32 -0300

    AT-DR4 quest8-B: progress

--- University/DR4/AT/quest8_b.py
+++ University/DR4/AT/quest8_b.py
@@ -16,3 +16,48 @@ def fatorial(n):
         b) usando o módulo threading com 4 threads;
         c) usando o módulo multiprocessing com 4 processos.
 '''
+
+import threading
+import random
+
+
+def somaThread(lista, soma_parcial, id):
+    soma = 0
+    for i in lista:
+        soma = soma + i
+    soma_parcial[id] = soma
+
+
+N = 10000
+
+# Captura tempo inicial
+t_inicio = float(time.time())
+
+# Gera lista com valores aleatórios
+lista = []
+
+for i in range(N):
+    lista.append(random.randint(-50, 51))
+
+Nthreads = 4  # Número de threads a ser criado
+
+# Vetor para salvar a soma parcial de cada thread
+soma_parcial = Nthreads * [0]
+lista_threads = []
+
+for i in range(Nthreads):
+    ini = i * int(N/Nthreads)  # início do intervalo da lista
+    fim = (i + 1) * int(N/Nthreads)  # fim do intervalo da lista
+
+t = threading.Thread(
+    target=somaThread,
+    args=(lista[ini:fim], soma_parcial, i))
+t.start()  # inicia thread
+
+lista_threads.append(t)  # guarda a thread
+
+for t in lista_threads:
+t.join()  # Espera as threads terminarem
+
+
+print_final_result(t_inicio, soma)
