598fecbcacb234b468c210a38546b73e67ab9c77
Alex Santos <arkanjo.alex@gmail.com>
2019-04-03 09:35:49 -0300

    REMOVE: Conflitos

--- University/DR4/etapa2-Modulo-PsUtil.py
+++
@@ -1,9 +0,0 @@
-import psutil, time
-
-print ( psutil.pids())  # todos os processos ativos
-
-cmdList = p.cwd()   # 
-pid = 543
-cmdLIst = psutil.pid_exists(pid)  # indica se existe
-
-print( psutil.process_iter())   # PID's - permite iterar com 'for' e etc.
